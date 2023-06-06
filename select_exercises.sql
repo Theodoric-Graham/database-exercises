d5a9b0142f537589895f49c9fd9c083b7975febb
Theodoric Graham <theodoricgraham@gmail.com>
2023-06-06 15:16:48 -0500

    added extra data to basic statement exercise, finished part 2 of exercise

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,5 +1,5 @@
 -- SELECT * FROM albums WHERE artist = 'Pink Floyd';
--- SELECT release_date FROM albums WHERE name = 'Sgt. Pepper\'s Lonely Hearts Club Band';
+-- SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 -- SELECT genre FROM albums WHERE name = 'Nevermind';
 -- SELECT * FROM albums WHERE release_date BETWEEN 1990 and 1999;
 -- SELECT * FROM albums WHERE sales < 20;
