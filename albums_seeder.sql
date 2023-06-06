f6968eb1fdc055c2c23fc3d00d9688d854277b45
Theodoric Graham <theodoricgraham@gmail.com>
2023-06-06 15:55:05 -0500

    finished part 4 of basic statements exercise

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,3 +1,4 @@
+TRUNCATE albums;
 INSERT INTO albums (artist, name, release_date, genre, sales)
 VALUES ('Michael Jackson', 'Thriller', 1982, 'Pop, post-disco, funk, rock', 51.2),
        ('AC/DC', 'Back in Black', 1980, 'Hard rock', 30.1),
