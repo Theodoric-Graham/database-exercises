61f766b3415762937909eea14bbbbd7d13089d8b
Theodoric Graham <theodoricgraham@gmail.com>
2023-06-07 14:52:51 -0500

    Started limit exercises

---
+++ limit_exercises.sql
@@ -0,0 +1,3 @@
+-- List the first 10 distinct last name sorted in descending order.
+SELECT DISTINCT last_name FROM employees ORDER BY last_name DESC LIMIT 10;
+-- Create a query to get the top 5 salaries and display just the employees number from the salaries table.
