5d5db7e5b9fc1ca8b02d803064d7e2776f470e8f
Theodoric Graham <theodoricgraham@gmail.com>
2023-06-09 11:58:12 -0500

    finished sub-queries exercises without bonus

--- joins_exercises.sql
+++ joins_exercises.sql
@@ -62,7 +62,7 @@ FROM users
 SELECT COUNT(role_id) AS role_count, roles.name AS role_name
 FROM users
          RIGHT JOIN roles ON users.role_id = roles.id
-GROUP BY role_id
+GROUP BY role_id;
 
 -- Employees Database
 -- Using the example in the Associative Table Joins section as a guide, write a query that shows each department along with the name of the current manager for that department.
@@ -90,29 +90,59 @@ WHERE dm.to_date = '9999-01-01'
   AND e.gender = 'F'
 GROUP BY d.dept_name;
 -- Find the current titles of employees currently working in the Customer Service department.
-SELECT COUNT(t.title) AS Total, t.title
-FROM employees as e
-         JOIN current_dept_emp as de
-              ON de.emp_no = e.emp_no
-         JOIN departments as d
-              ON d.dept_no = de.dept_no
-         JOIN titles as t
-              ON t.emp_no = e.emp_no
-WHERE t.to_date = '9999-01-01'
-  AND d.dept_name = 'Customer Service'
-GROUP BY t.title
-ORDER BY Total DESC;
+# SELECT COUNT(t.title) AS Total, t.title
+# FROM employees as e
+#          JOIN current_dept_emp as de
+#               ON de.emp_no = e.emp_no
+#          JOIN departments as d
+#               ON d.dept_no = de.dept_no
+#          JOIN titles as t
+#               ON t.emp_no = e.emp_no
+# WHERE t.to_date = '9999-01-01'
+#   AND d.dept_name = 'Customer Service'
+# GROUP BY t.title
+# ORDER BY Total DESC;
+-- review answer
+SELECT titles.title AS 'title', COUNT(*) AS 'total'
+FROM departments
+JOIN dept_emp on dept_emp.dept_no = departments.dept_no
+JOIN titles ON titles.emp_no = dept_emp.emp_no
+WHERE departments.dept_name = 'Customer Service' AND titles.to_date = '9999-01-01' AND dept_emp.to_date = '9999-01-01'
+GROUP BY title
+ORDER BY total DESC;
 -- Find the current salary of all current managers.
-SELECT d.dept_name AS Department_Name, CONCAT(e.first_name, ' ', e.last_name) AS Department_Manager, s.salary
-FROM employees as e
-         JOIN dept_emp as de
-              ON de.emp_no = e.emp_no
-         JOIN departments as d
-              ON d.dept_no = de.dept_no
-         JOIN dept_manager as dm
-              ON dm.emp_no = e.emp_no
-         JOIN salaries as s
-              ON s.emp_no = e.emp_no
+# SELECT d.dept_name AS Department_Name, CONCAT(e.first_name, ' ', e.last_name) AS Department_Manager, s.salary
+# FROM employees as e
+#          JOIN dept_emp as de
+#               ON de.emp_no = e.emp_no
+#          JOIN departments as d
+#               ON d.dept_no = de.dept_no
+#          JOIN dept_manager as dm
+#               ON dm.emp_no = e.emp_no
+#          JOIN salaries as s
+#               ON s.emp_no = e.emp_no
 # Where Current Manager AND Current Salary
-WHERE dm.to_date = '9999-01-01' AND s.to_date = '9999-01-01'
-GROUP BY d.dept_name;
+# WHERE dm.to_date = '9999-01-01' AND s.to_date = '9999-01-01'
+# GROUP BY d.dept_name;
+-- review answer
+SELECT departments.dept_name AS 'department name', CONCAT(employees.first_name, ' ', employees.last_name) AS 'Department Manager', salaries.salary AS 'Salary'
+FROM departments
+JOIN dept_manager ON departments.dept_no = dept_manager.dept_no
+JOIN employees on employees.emp_no = dept_manager.emp_no
+JOIN salaries ON employees.emp_no =  salaries.emp_no
+WHERE dept_manager.to_date LIKE '9999%' AND salaries.to_date LIKE '9999%'
+
+-- Bonus Find the names of all current employees, their department name, and their current manager's name.
+SELECT CONCAT(e.first_name, ' ', e.last_name) as Employee,
+       d.dept_name as Department,
+        CONCAT(e2.first_name, ' ', e2.last_name) as Manager
+FROM dept_emp as de
+JOIN employees as e ON de.emp_no = e.emp_no
+JOIN departments as d ON de.dept_no = d.dept_no
+JOIN dept_manager as dm on dm.dept_no = d.dept_no
+AND dm.to_date = '9999-01-01'
+JOIN employees as e2 on e2.emp_no = dm.emp_no
+where de.to_date = '9999-01-01'
+
+
+
