c0e42620800d04e124b63bb089118d82c1be65bf
Theodoric Graham <theodoricgraham@gmail.com>
2023-06-09 12:28:33 -0500

    finished sub-queries exercises with bonus

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -4,11 +4,11 @@ SELECT first_name, last_name, hire_date
 FROM employees
 WHERE hire_date IN (SELECT hire_date
                     FROM employees
-                    WHERE employees.emp_no = 101010)
+                    WHERE emp_no = 101010);
 -- Find all the titles held by all employees with the first name Aamod. 314 total titles, 6 unique titles
 SELECT title, COUNT(title)
 FROM titles
-WHERE titles.emp_no IN (
+WHERE emp_no IN (
     SELECT emp_no
     FROM employees
     WHERE first_name = 'Aamod'
@@ -26,8 +26,23 @@ WHERE emp_no IN (
 
 -- BONUS
 -- Find all the department names that currently have female managers.
-
+select d.dept_name
+from departments as d
+where d.dept_no IN
+(select dm.dept_no
+FROM dept_manager as dm
+JOIN employees e on dm.emp_no = e.emp_no
+where dm.to_date = '9999-01-01' and e.gender = 'F');
 -- Find the first and last name of the employee with the highest salary.
+select first_name, last_name
+FROM employees as e
+WHERE e.emp_no =
+(select s.emp_no
+from salaries as s
+ORDER BY salary DESC
+LIMIT 1);
+
+
 
 
 
